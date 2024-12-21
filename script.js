a046762d17e54de06fb6fbf6f341bdc0f3208755
Shyam <gshyamprasad2001@gmail.com>
2024-12-21 21:47:20 +0530

    finished

--- script.js
+++ script.js
@@ -15,23 +15,53 @@ async function getMovies(url){
      showMovies(data.results);
 }
 
-function showMovies(movies){
-    main.innerHTML="";
-    
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `;
+    main.appendChild(movieEl);
+  });
 }
 
- form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    let search = searchItem.value;
-    if(searchItem&& searchItem !== ''){
-        getMovies(SEARCH_API+search);
-        searchItem.value="";
- }else{
-        window.location.reload();
- }
-})
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
 
 
     
